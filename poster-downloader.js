9bed04d8993f69953b1ea27a073d69adbd42880b
Blake-sizemore <magnify1more@gmail.com>
2024-02-05 00:01:57 -0500

    Lesson Update - 02-04-2024

--- poster-downloader.js
+++ poster-downloader.js
@@ -1,23 +1,28 @@
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+// Downlaoding an image
+// images for fecth need to look at blob
 
 
 fetch(`https://api-ghibli.herokuapp.com/films`)
 .then(res => res.json())
 .then((films) => {
-  films.forEach(film => {
+  console.time('start')
+  films.forEach((film, i) => {
       let imageUrl  = `${film.movie_banner}`;
-      let imageLoad = `download/${film.title}.jpeg`;
+      let imageLoad = `download/${i}_${film.title}.jpeg`;
       let ext = path.extname(imageLoad);
       async function downloadImage(url, filename) {
         const response = await axios.get(url, { responseType: 'arraybuffer' });
 
         fs.writeFile(filename, response.data, (err) => {
           if (err) throw err;
+          // 
           console.log('successfully!', [filename, imageUrl],ext);
         });
       }
       downloadImage(imageUrl, imageLoad);
     });
   })
+  .catch(error => console.log(error));
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+// 
 const fs = require('fs');
+// way to interact with file system
+// both work together usually
 
 console.log("tester");
 
@@ -7,37 +10,48 @@ let chirpsArr1 = [{
     "uId": "e54eab15-b6ef-40c3-a68e-765c787ffd23",
     "user": "WhataTester2",
     "msg": "This is a test #2",
-    "create_at": 1705974198463
+    "created_at": 1705974198463
 }, {
     "uId": "7f53f06d-bad0-48b6-b41e-acca9273882a",
     "user": "tester1",
     "msg": "This tester # 1",
-    "create_at": 1705974171906
+    "created_at": 1705974171906
 }, {
     "uId": "x20D88c1-79a0-d85a-43cd-0c9f6cc219c7",
     "user": "BitsandBytes",
     "msg": "Who is left over?",
-    "create_at": 1705969826683
+    "created_at": 1705969826683
 }, {
     "uId": "A20088c1-79a0-d85a-43cd-0c9f6cc219c7",
     "user": "AllTheKingsHorses",
     "msg": "Peat falls off and into the water",
-    "create_at": 1705969811683
+    "created_at": 1705969811683
 }, {
     "uId": "550088c1-79a0-d85a-43cd-0c9f6cc219c7",
     "user": "HumptyDumpty10",
     "msg": "Peat and Repeat are in a boat together",
-    "create_at": 1705969726683
-}];
+    "created_at": 1705969726683
+}].map(chirp => {
+    const formattedTime = new Date(chirp.created_at).toLocaleString()
+    return {
+        ...chirp,
+        formattedTime
+    }
+})
 
 console.log(chirpsArr1);
 
-const chirpsRun = path.join(__dirname,'../chirpArrSet.json');
+const chirpsRun = path.join(__dirname, '../chirpArrSet.json');
+// 
 
-fs.writeFile(chirpsRun,JSON.stringify(chirpsArr1),(err) => {
-    if(err) {
+fs.writeFile(chirpsRun, JSON.stringify(chirpsArr1), (err) => {
+    if (err) {
         console.log(err);
-    }});
+    }
+});
+// Write does create the file you need
+// first add location
+// turn your code into JOSN with stringify
 
 var filmArr = [];
 const favFilmRun = path.join(__dirname, '../favorite-films.json');
@@ -53,12 +67,12 @@ fetch('http://api-ghibli.herokuapp.com/films')
                 "image": `${film.image}`,
                 "description": `${film.description}`
 
-            }),
-            fs.writeFile(favFilmRun, JSON.stringify(filmArr), (err) => {
-                if (err) {
-                    console.log(err);
-                }
             })
         ))
+        fs.writeFile(favFilmRun, JSON.stringify(filmArr), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
     })
     .catch(e => (e.message))
